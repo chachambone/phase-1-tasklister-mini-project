0a39c18b12e04f9ed1ac5f57637b0a9fba96886e
Marycharity <marycharity5@gmail.com>
2025-03-24 23:32:58 +0300

    Intial add task implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("create-task-form");
+  const taskInput = document.getElementById('new-task-description');
 
   form.addEventListener("submit",(event)=>{
     event.preventDefault();
-    console.log("test")
+    
+    const description = taskInput.value;
+    addTask(description)
+    form.reset();
+
   })
 });
+
+function addTask(description){
+  console.log(description)
+}
