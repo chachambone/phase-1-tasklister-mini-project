761664d214e1e22809b480d6c3e9e4d4ad58dc79
Marycharity <marycharity5@gmail.com>
2025-03-24 23:25:35 +0300

    Update click listener on button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    console.log("test")
+  })
 });
