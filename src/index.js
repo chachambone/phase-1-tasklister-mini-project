3b613a666accf32f9c0a7c2f9f6434299c7e4bc5
Marycharity <marycharity5@gmail.com>
2025-03-26 21:20:15 +0300

    Delete an item from the list

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("create-task-form");
-  const taskInput = document.getElementById('new-task-description');
-  const list = document.getElementById('tasks')
+  const taskInput = document.getElementById('new-task-description');  
 
   form.addEventListener("submit",(event)=>{
     event.preventDefault();
@@ -17,9 +16,20 @@ function addTask(description){
   let tasks = [];
   const list = document.getElementById('tasks')
   const taskItem = document.createElement("li");
+
   taskItem.textContent = description
+  deleteItem(taskItem)
 
   tasks.push(description)
   list.appendChild(taskItem)
 
+}
+
+function deleteItem(taskItem){
+  const deleteButton = document.createElement("button")
+  deleteButton.textContent = "X"
+  deleteButton.addEventListener("click",()=>{
+    taskItem.remove();
+  });
+  taskItem.appendChild(deleteButton);
 }
